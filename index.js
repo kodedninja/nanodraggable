43c805d9173a3ddf2091f663734a72758fee3bcd
Hunor Karamán <hunix94@gmail.com>
2018-07-23 14:34:45 +0300

    changes:

--- index.js
+++ index.js
@@ -40,9 +40,10 @@ Nanodraggable.prototype.createElement = function () {
 
 		var rect = t.element.getBoundingClientRect()
 		t.offset_x = e.clientX - rect.left
-		t.offset_y = e.clientY - rect.top
+		t.offset_y = e.clientY - rect.top + 36 // fix this!
 
 		document.addEventListener('mousemove', move)
+		t.mousedown()
 	}
 
 	function move(e) {
@@ -50,16 +51,23 @@ Nanodraggable.prototype.createElement = function () {
 		t.moving = true
 
 		if (t.clicked) {
-			t.x = e.clientX
-			t.y = e.clientY
-			t.rerender()
+			t.x = e.clientX - t.offset_x
+			t.y = e.clientY - t.offset_y
+
+			t.element.style.top = t.y + 'px'
+			t.element.style.left = t.x + 'px'
 		}
 	}
 
 	function up(e) {
 		t.moving = false
 		t.clicked = false
+		t.mouseup()
 	}
 }
+
+// to override
+Nanodraggable.prototype.mousedown = function () {}
+Nanodraggable.prototype.mouseup = function () {}
 
 module.exports = Nanodraggable
